0dc593391f241c672611d6e8c7cff557d8dc056f
SalinasAlan <alansal_11@hotmail.com>
2017-09-29 23:30:23 -0500

    Se agrega el paquete de la pila

---
+++ src/edd/pila/Expresiones.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd.pila;
+
+/**
+ *
+ * @author Salinas
+ */
+public class Expresiones
+{
+
+    public void pos(char arr[])
+    {
+        Pila op = new Pila(arr);
+        Pila var = new Pila(arr);
+        
+        for (int i = 0; i < arr.length; i++)
+        {
+            //Ingresa las variables
+            if (arr[i] > 'a' || arr[i] < 'z')
+            {
+                var.push(arr[i]);           
+            }
+            //Ingresa la resta
+            if (arr[i] == '-')
+            {
+                if (!op.empty())
+                {
+                    if (op.stackTop() == '*')
+                    {
+                        var.push(op.pop());
+                        op.push(arr[i]);
+                    }
+                }
+            }
+            //Ingresa la multiplicacion
+            if (arr[i] == '*')
+            {
+                if (!op.empty())
+                {
+                    if (op.stackTop() == '-')
+                    {
+                        op.push(arr[i]);
+                    }
+                }
+            }
+            
+        }
+    }
+
+}
