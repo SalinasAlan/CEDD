2434af49fdf568ed8fc0cfdfe48e324af68ac135
SalinasAlan <alansal_11@hotmail.com>
2017-10-07 23:03:20 -0500

    Se agregó un segundo método de pos en la clase Expresiones

--- src/edd/pila/Expresiones.java
+++ src/edd/pila/Expresiones.java
@@ -18,24 +18,42 @@ public class Expresiones
      *
      * @param arr es un arreglo te tipo char.
      */
-    public void pos(char arr[])
+    public char[] pos(char arr[])
     {
         Pila op = new Pila(arr.length);
         Pila var = new Pila(arr.length);
 
         for (int i = 0; i < arr.length; i++)
         {
+            //Ingresa la suma.
+            if (arr[i] == '+')
+            {
+                if (!op.empty())
+                {
+                    if (op.stackTop() == '*' || op.stackTop() == '/' || op.stackTop() == '-' || op.stackTop() == '+')
+                    {
+                        var.push(op.pop());
+                        op.push(arr[i]);
+                    } else
+                    {
+                        op.push(arr[i]);
+                    }
+                } else
+                {
+                    op.push(arr[i]);
+                }
+            }
             //Ingresa la resta.
             if (arr[i] == '-')
             {
                 if (!op.empty())
                 {
-                    if (op.stackTop() == '*')
+                    if (op.stackTop() == '*' || op.stackTop() == '/' || op.stackTop() == '-' || op.stackTop() == '+')
                     {
                         var.push(op.pop());
                         op.push(arr[i]);
                     }
-                    if (op.stackTop() == (char)40)
+                    if (op.stackTop() == (char) 40)
                     {
                         op.push(arr[i]);
                     }
@@ -49,15 +67,11 @@ public class Expresiones
             {
                 if (!op.empty())
                 {
-                    if (op.stackTop() == '-')
+                    if (op.stackTop() == '*' || op.stackTop() == '/' || op.stackTop() == (char) 40)
                     {
                         op.push(arr[i]);
                     }
-                    if (op.stackTop() == '+')
-                    {
-                        op.push(arr[i]);
-                    }
-                    if (op.stackTop() == (char)40)
+                    if (op.stackTop() == '-' || op.stackTop() == '+')
                     {
                         op.push(arr[i]);
                     }
@@ -65,15 +79,20 @@ public class Expresiones
                 {
                     op.push(arr[i]);
                 }
+
             }
             //Ingresa la división.
             if (arr[i] == '/')
             {
                 if (!op.empty())
                 {
-                    if (op.stackTop() == '*')
+                    if (op.stackTop() == '*' || op.stackTop() == '/' || op.stackTop() == (char) 40)
                     {
                         var.push(op.pop());
+                        op.push(arr[i]);
+                    }
+                    if (op.stackTop() == '-' || op.stackTop() == '+')
+                    {
                         op.push(arr[i]);
                     }
                 } else
@@ -109,24 +128,7 @@ public class Expresiones
             {
                 var.push(arr[i]);
             }
-            //Ingresa la suma.
-            if (arr[i] == '+')
-            {
-                if (!op.empty())
-                {
-                    if (op.stackTop() == '*')
-                    {
-                        var.push(op.pop());
-                        op.push(arr[i]);
-                    } else
-                    {
-                        op.push(arr[i]);
-                    }
-                } else
-                {
-                    op.push(arr[i]);
-                }
-            }
+
         }
 
         //Al finalizar la división de los términos, todo se pasa a la pila de las variables 
@@ -140,6 +142,87 @@ public class Expresiones
             {
                 var.push(op.pop());
             }
-        } 
+        }
+        return var.getArr();
+    }
+
+    
+    
+    
+    
+    /**
+     *
+     * @param s
+     */
+    public char[] pos2(String s)
+    {
+        Pila operadores = new Pila(s.length());
+        Pila variables = new Pila(s.length());
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (Character.isLetter(s.charAt(i)))
+            {
+                variables.push(s.charAt(i));
+            } else if (!operadores.empty())
+            {
+                switch (s.charAt(i))
+                {
+                    case (char) 40:
+                        if (operadores.stackTop() == '*' || operadores.stackTop() == '/')
+                        {
+                            variables.push(operadores.pop());
+                            operadores.push(s.charAt(i));
+                        } else
+                        {
+                            operadores.push(s.charAt(i));
+                        }
+                        break;
+                    case (char) 41:
+                        operadores.push(s.charAt(i));
+                        break;
+                    case '+':
+                    case '-':
+                        if (operadores.stackTop() == '*' || operadores.stackTop() == '/' || operadores.stackTop() == '-' || operadores.stackTop() == '+')
+                        {
+                            variables.push(operadores.pop());
+                            operadores.push(s.charAt(i));
+                        } else
+                        {
+                            operadores.push(s.charAt(i));
+                        }
+                        break;
+
+                    case '/':
+                    case '*':
+                        if (operadores.stackTop() == '*' || operadores.stackTop() == '/' )
+                        {
+                            variables.push(operadores.pop());
+                            operadores.push(s.charAt(i));
+                        }
+                        if (operadores.stackTop() == '-' || operadores.stackTop() == '+' || operadores.stackTop() == (char) 40)
+                        {
+                            operadores.push(s.charAt(i));
+                        }
+                        break;
+                }
+            } else
+            {
+                operadores.push(s.charAt(i));
+            }
+        }
+
+        while (operadores.getTope() != -1)
+        {
+            if (operadores.stackTop() == (char) 40 || operadores.stackTop() == (char) 41)
+            {
+                operadores.pop();
+            } else
+            {
+                variables.push(operadores.pop());
+            }
+        }
+        
+        return variables.getArr();
     }
 }
