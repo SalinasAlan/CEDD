b966b49815342c86ff6615d5fce444c2e1cc5b93
SalinasAlan <alansal_11@hotmail.com>
2017-10-18 18:35:56 -0500

    Se agregó la estructura Lista que se comporta como Pila

--- src/edd/listas/Lista.java
+++ src/edd/listas/Lista.java
@@ -9,7 +9,44 @@ package edd.listas;
  *
  * @author Salinas
  */
-public class Lista
+public class Lista //se comporta como una Pila
 {
-    
+
+    private Alumno mTope;
+
+    public boolean empty()
+    {
+        return mTope == null;
+    }
+
+    public void push(Alumno mObjA)
+    {
+        if (empty())
+        {
+            mTope = mObjA;
+        } else
+        {
+            mObjA.setmSig(mTope);
+            mTope = mObjA;
+        }
+    }
+
+    public Alumno pop()
+    {
+        Alumno mAux = mTope;
+        mTope = mTope.getmSig();
+        return mAux;
+    }
+
+    public void dep()
+    {
+        Alumno mAux = mTope;
+        while (mAux != null)
+        {
+            System.out.println("\nEl nombre es: " + mAux.getmNombre());
+            System.out.println("El promedio es: " + mAux.getmProm());
+            System.out.println("El No. de control es: " + mAux.getmNoControl());
+            mAux = mAux.getmSig();
+        }
+    }
 }
